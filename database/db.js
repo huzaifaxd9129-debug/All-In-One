a5e4996f1a924fb4855efb0050fcd15ac884e7c1
huzaifaxd9129-debug <huzaifaxd9129@gmail.com>
2026-05-05 19:54:54 +0500

    db.js

--- database/db.js
+++ database/db.js
@@ -1,5 +1,41 @@
-const { QuickDB } = require("quick.db");
+const fs = require("fs");
 
-const db = new QuickDB();
+const file = "./database/data.json";
 
-module.exports = db
+// create file if not exists
+if (!fs.existsSync(file)) {
+  fs.writeFileSync(file, "{}");
+}
+
+function read() {
+  return JSON.parse(fs.readFileSync(file));
+}
+
+function write(data) {
+  fs.writeFileSync(file, JSON.stringify(data, null, 2));
+}
+
+module.exports = {
+  get: (key) => {
+    const data = read();
+    return data[key];
+  },
+
+  set: (key, value) => {
+    const data = read();
+    data[key] = value;
+    write(data);
+  },
+
+  add: (key, value) => {
+    const data = read();
+    data[key] = (data[key] || 0) + value;
+    write(data);
+  },
+
+  delete: (key) => {
+    const data = read();
+    delete data[key];
+    write(data);
+  },
+};
