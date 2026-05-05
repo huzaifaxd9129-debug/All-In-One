919dad1733dc35b5dabeed75a172946e8e34f089
huzaifaxd9129-debug <huzaifaxd9129@gmail.com>
2026-05-05 19:24:14 +0500

    guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,1 +1,20 @@
+const config = require("../config");
+const { EmbedBuilder } = require("discord.js");
 
+module.exports = {
+  name: "guildMemberAdd",
+
+  execute(member) {
+    const channel = member.guild.channels.cache.get(config.welcomeChannel);
+    if (!channel) return;
+
+    const embed = new EmbedBuilder()
+      .setColor(config.color || "#2f3136")
+      .setTitle("👋 Welcome!")
+      .setDescription(`Welcome ${member} to **${member.guild.name}**`)
+      .setThumbnail(member.user.displayAvatarURL())
+      .setFooter({ text: "Enjoy your stay!" });
+
+    channel.send({ embeds: [embed] }).catch(() => {});
+  },
+};
