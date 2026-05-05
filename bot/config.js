a11bc48f46e358c45d2a07a52eaaf0dfff55e851
huzaifaxd9129-debug <huzaifaxd9129@gmail.com>
2026-05-05 19:20:46 +0500

    config.js

--- bot/config.js
+++ bot/config.js
@@ -1,6 +1,25 @@
 module.exports = {
+  // ================= BASIC =================
   prefix: "-",
+  ownerID: "YOUR_ID_HERE",
 
+  // ================= CHANNEL IDS =================
   logsChannel: "1500169350307647488",
   welcomeChannel: "1500169317747261490",
+
+  // ================= SYSTEM SETTINGS =================
+  ticketCategoryName: "Tickets",
+  applyChannel: "1500399315707887756",
+
+  // ================= ECONOMY =================
+  startingCoins: 100,
+
+  // ================= BOT STATUS =================
+  status: "Made By Huztro",
+
+  // ================= DASHBOARD =================
+  dashboardPort: 3000,
+
+  // ================= OPTIONAL =================
+  color: "#2f3136"
 };
