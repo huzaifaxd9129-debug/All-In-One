b3b9bb1ea37f53ede7fc81a0eeb38c3e0438aebc
huzaifaxd9129-debug <huzaifaxd9129@gmail.com>
2026-05-05 19:31:35 +0500

    welcome.js

--- systems/welcome.js
+++ systems/welcome.js
@@ -1,1 +1,25 @@
+const { EmbedBuilder } = require("discord.js");
+const config = require("../config");
 
+module.exports = (client) => {
+
+  client.on("guildMemberAdd", async (member) => {
+
+    const channel = member.guild.channels.cache.get(config.welcomeChannel);
+    if (!channel) return;
+
+    const embed = new EmbedBuilder()
+      .setTitle("👋 Welcome to the Server!")
+      .setDescription(
+        `Hey ${member.user}, welcome to **${member.guild.name}** 🎉\n\n` +
+        `You are member **#${member.guild.memberCount}**`
+      )
+      .setThumbnail(member.user.displayAvatarURL({ dynamic: true }))
+      .setColor(config.color || "#2f3136")
+      .setFooter({ text: "Enjoy your stay ❤️" })
+      .setTimestamp();
+
+    channel.send({ embeds: [embed] }).catch(() => {});
+  });
+
+};
